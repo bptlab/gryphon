54df2d85eaf066dcbcb2523cb1c01f0e3bb3bb9d
Alexander Kastius <alexander.kastius@student.hpi.de>
2015-11-24 14:20:03 +0100

    Added new modules (Bootstrap and react) for ui development. Added editor js to start develop the UI.

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/fragment', fragment);
 app.use('/api/scenario', scenario);
-app.use('/api/domainmodel', domainmodel);s
+app.use('/api/domainmodel', domainmodel);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
