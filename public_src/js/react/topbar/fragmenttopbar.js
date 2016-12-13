a257614d5c900bffb23ab24fda35b05b186bf5f2
Adrian Holfter <adrian.holfter@student.hpi.uni-potsdam.de>
2016-12-13 16:20:01 +0100

    Revert "TopBar rename buttons are now white instead of green"
    This reverts commit d7e05e6a7b3433869a006e783555278973829d3f.

--- public_src/js/react/topbar/fragmenttopbar.js
+++ public_src/js/react/topbar/fragmenttopbar.js
@@ -88,7 +88,7 @@ var FragmentTopBarComponent = React.createClass({
                 <div className="btn-group pull-right">
                   <button
                       type="button"
-                      className="btn btn-default"
+                      className="btn btn-success"
                       onClick={this.handleRenameClick}
                   >
                       <i className="fa fa-pencil"></i> {this.state.nameIsEditable ? "Done" : "Edit"}
