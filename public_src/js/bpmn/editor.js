18c67fec1f219624390afb9a2cc9785a325e84b2
Adrian Holfter <adrian.holfter@student.hpi.uni-potsdam.de>
2017-06-01 16:47:58 +0200

    Trim whitespace from dataclass and state name in data object references. Closes #9

--- public_src/js/bpmn/editor.js
+++ public_src/js/bpmn/editor.js
@@ -60,14 +60,15 @@ Editor.prototype.handleChange = function(event, object) {
     if (object.element.businessObject != null) {
         var bo = object.element.businessObject;
         if (bo.$type == "bpmn:DataObjectReference") {
-            console.log(bo);
             if (bo.name != null) {
                 var end = bo.name.indexOf("[");
                 var realend = bo.name.indexOf("]");
                 if (end >= 0 && realend >= 0 && realend > end) {
-                    bo.dataclass = bo.name.substring(0,end);
-                    bo.state = bo.name.substring(end+1,realend);
+                    bo.dataclass = bo.name.substring(0,end).trim();
+                    bo.state = bo.name.substring(end+1,realend).trim();
                 }
+                // Re-generate name (to discard trimmed whitespace)
+                bo.name = bo.dataclass + "[" + bo.state + "]";
             }
         }
     }
