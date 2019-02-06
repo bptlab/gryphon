b2d29a673e7d9d8b2c0452d2410ac86dbbe2de24
MaximilianV <maximilian-voelker@t-online.de>
2019-02-06 12:00:49 +0100

    Start a resource manager api and use it to fetch available resource types

--- public_src/js/react/domainmodeleditor/typeselect.js
+++ public_src/js/react/domainmodeleditor/typeselect.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var ResourceAPI = require('./../../resourceApi');
 
 var TypeSelect = React.createClass({
     getInitialState: function () {
@@ -6,6 +7,13 @@ var TypeSelect = React.createClass({
             isEvent: this.props.is_event,
             isResource: this.props.is_resource
         };
+    },
+    componentDidMount: function () {
+        ResourceAPI.getAvailableResourceTypes(function (data) {
+            if (this.isMounted()) {
+                this.setState({ 'availableResourceTypes': data["resources"] });
+            }
+        }.bind(this));
     },
     handleChange: function (event) {
         if (event.target.value === "isEvent") {
@@ -18,7 +26,8 @@ var TypeSelect = React.createClass({
             this.setState({ isResource: isResource });
             this.props.handleType(this.state.isEvent, isResource);
         }
-        
+        console.log(this.state);
+
     },
     componentWillReceiveProps: function (nextProps) {
         this.setState({
@@ -29,26 +38,38 @@ var TypeSelect = React.createClass({
     render: function () {
         var value = this.state.value;
         return (
-            <div className="checkbox">
-                <label>
-                    <input
-                        type="checkbox"
-                        value="isEvent"
-                        checked={this.state.isEvent}
-                        onChange={this.handleChange}
-                    />
-                    Use as event type
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        value="isResource"
-                        checked={this.state.isResource}
-                        onChange={this.handleChange}
-                    />
-                    Use as a resource
-                </label>
+            <div>
+                <div className="checkbox">
+                    <label>
+                        <input
+                            type="checkbox"
+                            value="isEvent"
+                            checked={this.state.isEvent}
+                            onChange={this.handleChange}
+                        />
+                        Use as event type
+                    </label>
+                    <label>
+                        <input
+                            type="checkbox"
+                            value="isResource"
+                            checked={this.state.isResource}
+                            onChange={this.handleChange}
+                        />
+                        Use as a resource
+                    </label>
+                </div>
+                {this.state.isResource && this.state.availableResourceTypes && 
+                    <div>
+                        <select>
+                        {this.state.availableResourceTypes.map(type => {
+                            return(<option key={type["id"]} value={type["id"]}>{type["name"]}</option>);
+                        })}
+                        </select>
+                    </div>
+                }
             </div>
+
         );
     }
 });
