da0519f403545f6c0322162082fe853bb6514977
MaximilianV <maximilian-voelker@t-online.de>
2019-06-19 14:09:38 +0200

    Fix indentation and whitespace issues in validator class

--- server_src/validators/validator.js
+++ server_src/validators/validator.js
@@ -28,12 +28,12 @@ var GeneralValidator = class {
      * @param initDone {function} A function that should get called when the DB-initiation is done.
      * @param validators {array} A list of Classes that should be used as validator (Event Soundness and OLC on Default)
      */
-    constructor(fragment,initDone, validators) {
+    constructor(fragment, initDone, validators) {
         if (validators == undefined) {
             validators = [BoundValidator, EventValidator, SoundnessValidator, OLCValidator, DataObjectReferenceValidator, ThrowEventValidator];
         }
         if (initDone == undefined) {
-            initDone = function() {
+            initDone = function () {
                 this.validateEverything();
             }
         }
@@ -41,13 +41,13 @@ var GeneralValidator = class {
         this.fragment = fragment;
         this.bpmnObject = parseToBPMNObject(fragment.content);
         this.messages = [];
-        Scenario.findOne({fragments:fragment._id}).populate('domainmodel').exec(function(err, result) {
+        Scenario.findOne({ fragments: fragment._id }).populate('domainmodel').exec(function (err, result) {
             if (result == null) {
                 throw "Can't find domainmodel for fragment";
             }
             this.scenario = result;
             this.olc = parseOLCPaths(result.domainmodel);
-	    this.dm = result.domainmodel;
+            this.dm = result.domainmodel;
             initDone();
         }.bind(this))
     }
@@ -58,7 +58,8 @@ var GeneralValidator = class {
      */
     async validateEverything() {
         const runningValidators = [];
-        this.validators.forEach(function(validator){
+
+        this.validators.forEach(function (validator) {
             if (validator == BoundValidator) {
                 validator = new validator(this.fragment)
             } else if (validator == OLCValidator) {
@@ -66,14 +67,14 @@ var GeneralValidator = class {
             } else if (validator == DataObjectReferenceValidator) {
                 validator = new validator(this.fragment.preconditions, this.olc);
             } else if (validator == ThrowEventValidator) {
-      				validator = new validator(this.bpmnObject, this.dm)
-      			} else {
+                validator = new validator(this.bpmnObject, this.dm)
+            } else {
                 validator = new validator(this.bpmnObject);
             }
             runningValidators.push(validator.validateEverything());
         }.bind(this));
+
         const resolvedValidators = await Promise.all(runningValidators);
-        
         resolvedValidators.forEach(function (validatorResult) {
             this.messages = this.messages.concat(validatorResult);
         }.bind(this));
